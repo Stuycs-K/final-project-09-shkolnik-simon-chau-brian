6b5a6623a91715a890af0c837a4ef751a66e03ee
Br1anChau <bchau40@stuy.edu>
2024-05-23 18:38:41 -0400

    Worked on merging encoded audio back into original audio

--- encode.py
+++ encode.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.signal import stft
+from scipy.signal import istft
 import matplotlib.pyplot as plt
 import audiofile
 
@@ -11,8 +12,10 @@ file = open("testfile.txt", "r")
 byteArray = bytearray(file.read(), "utf-8")
 
 i = 0
-portion = signal[sampling_rate * 0 : sampling_rate * 20]
+newAudio = []
 while(i * 20 < len(byteArray)):
+    portion = signal[sampling_rate * 0 : sampling_rate * 20]
+    signal = signal[sampling_rate * 20 : sampling_rate * 40]
     f, t, Zxx = stft(portion, fs = sampling_rate, nperseg = 4096, noverlap = 0, nfft = 8192)
     Zxx = np.absolute(Zxx)
     nextbyte = 0
@@ -22,4 +25,7 @@ while(i * 20 < len(byteArray)):
         nextbyte += 1
     for x in Zxx:
         print(x[216])
-    i += 1  
+    portion = istft(Zxx, fs = sampling_rate, nperseg = 4096, noverlap = 0, nfft = 8192)
+    newAudio = newAudio + portion
+    i += 1
+newAudio = newAudio + signal 
