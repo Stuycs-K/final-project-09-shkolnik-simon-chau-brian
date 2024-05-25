f689261f5c583ba9fcc136ea0ff25e6ee7ae76d8
Br1anChau <bchau40@stuy.edu>
2024-05-25 07:39:48 -0400

    Trying to figure out how to turn get frequencies from Fourier transform.

--- decode.py
+++ decode.py
@@ -8,22 +8,22 @@ signal, sampling_rate = audiofile.read("modified_sample_input.mp3")
 # print(f"Sampling rate: {sampling_rate}hz")
 bitArray = [255] * 1000
 i = 0
-while(len(signal) < 20):
+while(len(signal) > 20):
     portion = signal[sampling_rate * 0 : sampling_rate * 20]
     signal = signal[sampling_rate * 20 : sampling_rate * 40]
     f, t, Zxx = stft(portion, fs = sampling_rate, nperseg = 4096, noverlap = 0, nfft = 8192)
     Zxx = np.absolute(Zxx)
     nextbit = 0
-    
-    for j in Zxx: 
-        f_index = np.argmin(np.abs(j - 20000))
-        if(Zxx[f_index] == 19000):
-            bitArray[nextbit] = 0
-            nextbit += 1
-        if(Zxx[f_index == 20000]):
-            bitArray[nextbit] = 1
-            nextbit += 1
-        print(bitArray[nextbit])
+    f_index = np.argmin(np.abs(Zxx[0] - 19000))
+    high_index = np.argmin(np.abs(Zxx[0] - 20000))
+    print(Zxx[0][f_index])
+    # if(frequency < 20000 and frequency >= 19000):
+    #     bitArray[nextbit] = 0
+    #     nextbit += 1
+    #     print(0)
+    # elif(frequency >= 20000):
+    #     bitArray[nextbit] = 1
+    #     nextbit += 1
         
     # Code with minimal libraries 
     # for j in Zxx:
