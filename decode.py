c4ed5a6081836c1d9773088c4d135fc92f29e3ab
Br1anChau <bchau40@stuy.edu>
2024-05-25 14:01:15 -0400

    Figured out how Zxx array worked but still did not get decoder working

--- decode.py
+++ decode.py
@@ -11,7 +11,7 @@ try:
     signal = sum(signal)
 except:
     print("Only one channel")
-bitArray = [255] * 1000
+bitArray = [3] * 10000
 i = 0
 
 while(len(signal) > 20):
@@ -20,25 +20,30 @@ while(len(signal) > 20):
     f, t, Zxx = stft(portion, fs = sampling_rate, nperseg = 4096, noverlap = 0, nfft = 8192)
     Zxx = np.absolute(Zxx)
     nextbit = 0
-    index = np.argmin(np.abs(f - 19000))
-    # f_index = np.argmin(np.abs(Zxx[0] - 19000))
-    # high_index = np.argmin(np.abs(Zxx[0] - 20000))
-    # print(1/Zxx[0][f_index])
-
+    oneindex = np.argmin(np.abs(f - 20000))
+    zeroindex = np.argmin(np.abs(f - 20000))
+    # Zxx = np.transpose(Zxx)
+    Zxx[oneindex].sort()
+    Zxx[zeroindex].sort()
+    for x in Zxx[zeroindex]:
+        print(x)
     # for x in f:
     #     print(x)
-
-    # if(frequency < 20000 and frequency >= 19000):
-    #     bitArray[nextbit] = 0
+    # oneStartIndex = 1
+    # zeroStartIndex = 1
+    # print(len(Zxx[oneindex]))
+    # while (oneStartIndex < 217 and zeroStartIndex < 217):
+    #     if(Zxx[oneindex][oneStartIndex] < Zxx[zeroindex][zeroStartIndex]):
+    #         bitArray[nextbit] = 1
+    #         oneStartIndex += 1
+    #     else:
+    #         bitArray[nextbit] = 0
+    #         zeroStartIndex += 1
     #     nextbit += 1
-    #     print(0)
-    # elif(frequency >= 20000):
-    #     bitArray[nextbit] = 1
-    #     nextbit += 1
-    i += 1
+    # i += 1
 
 for x in bitArray:
-    if(x != 255):
+    if(x != 3):
         print(x)
 decodedString = ""
 
