39ddae7ff671186aa36dfd223f57a59b2862c6a4
Simon Shkolnik <simonjs830@gmail.com>
2024-05-28 14:50:09 -0400

    STFT WORKS OMG

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import numpy as np
-from scipy.signal import stft
+from scipy.fft import fft, fftfreq
 import matplotlib.pyplot as plt
 import audiofile
 from utils import string_to_bin
@@ -23,22 +23,27 @@ signal = sum(signal) #combine all channels in audio file
 print(f"Audio length: {len(signal) / sampling_rate:.2f}s, taking segment from {SECTION[0]}s-{SECTION[1]}s")
 signal = signal[sampling_rate * SECTION[0] : sampling_rate * SECTION[1]] #take only section of audio
 
-def get_stft(signal):
-  #f, t, Zxx = stft(signal, fs = sampling_rate, nperseg = 4096, noverlap = 4096 - 128, nfft = 8192)
-  f, t, Zxx = stft(signal, fs = sampling_rate, nperseg = NPERSEG, noverlap = 0, nfft = NFFT)
-  Zxx = np.absolute(Zxx) #we only care about the magnitude of each frequency, not shift
+def get_stft(signal: np.ndarray):
+  t_signal = np.resize(signal, (len(signal) // NPERSEG) * NPERSEG)
+  t_signal = np.reshape(t_signal, (-1, NPERSEG))
 
-  #chop of frequencies > 10000
-  #f_indexes = f < 250000
-  #f = f[f_indexes]
-  #Zxx = Zxx[f_indexes]
+  f = fftfreq(NFFT, 1 / sampling_rate)[:NFFT // 2]
+  t = np.arange(0, len(signal), NPERSEG) / sampling_rate
+  Zxx = fft(t_signal, NFFT, axis=1)[:, :NFFT // 2]
+
+  Zxx = np.absolute(Zxx)
+  Zxx = np.transpose(Zxx)
+  Zxx = Zxx[:-1, :]
+
+  #f, t, Zxx = stft(signal, fs = sampling_rate, nperseg = NPERSEG, noverlap = 0, nfft = NFFT)
+  #Zxx = np.absolute(Zxx) #we only care about the magnitude of each frequency, not shift
 
   return f, t, Zxx
 
 _, (ax1, ax2) = plt.subplots(1, 2, sharey=True)
 
 f, t, Zxx = get_stft(signal)
-ax1.pcolorfast(t, f, np.abs(Zxx))
+ax1.pcolormesh(t, f, np.abs(Zxx))
 ax1.set_title("Spectrogram (before encoding)")
 ax1.set_ylabel("Frequency [Hz]")
 ax1.set_xlabel("Time [sec]")
@@ -61,7 +66,7 @@ for i, v in enumerate(bin_data):
   ax2.hlines(freq, t[i], t[i + 1], "r")
 
 f, t, Zxx = get_stft(signal)
-ax2.pcolorfast(t, f, np.abs(Zxx))
+ax2.pcolormesh(t, f, np.abs(Zxx))
 ax2.set_title("Spectrogram (after encoding)")
 ax2.set_ylabel("Frequency [Hz]")
 ax2.set_xlabel("Time [sec]")
