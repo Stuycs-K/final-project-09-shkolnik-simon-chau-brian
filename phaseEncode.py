49057c02f804a147d93edae09f0a7b5a5fd64804
Brian Chau <bchau40@stuy.edu>
2024-05-30 13:50:10 -0400

    Made phase encode but the change if obvious

--- phaseEncode.py
+++ phaseEncode.py
@@ -1,16 +1,14 @@
 import numpy as np
-from scipy.signal import stft
-from scipy.io import wavfile
+from scipy.fft import fft, fftfreq
 import matplotlib.pyplot as plt
 import audiofile
-from utils import string_to_bin, get_stft, read_audio
+from utils import read_audio, string_to_bin, get_stft, NPERSEG, NFFT
 
 AUDIO_FILE_NAME = "sample_input.wav"
 TEXT_FILE_NAME = "testfile.txt"
 
 SECTION = (20, 40)
-NPERSEG = 4096
-NFFT = 8192
+AMPLITUDE = 0.2
 
 FREQ_0 = 1500
 string = open(TEXT_FILE_NAME, "rb").read()
@@ -19,27 +17,19 @@ stringlen = len(string) * 8
 phaseShifts = np.empty(stringlen)
 index = 0
 for i, v in enumerate(binary):
-    if v == '0': phaseShifts[index] = np.pi/2
-    elif v == '1': phaseShifts[index] = -np.pi/2
-    index += 1
-
+    if v == '0': phaseShifts[i] = np.pi/2
+    elif v == '1': phaseShifts[i] = -np.pi/2
 signal, sampling_rate = read_audio(AUDIO_FILE_NAME)
 f, t, Zxx = get_stft(signal, sampling_rate)
-index = np.argmin(np.abs(f - FREQ_0))
-magnitudes = np.abs(Zxx[FREQ_0])
-phases = np.angle(Zxx[FREQ_0])
-phaseDiff = np.diff(phases, axis=0)
-print(magnitudes)
-print(phases)
 
-for x in range(len(phaseShifts)):
-    phases[x] += phaseShifts[x]
-# phases[0, halfChunk - stringlen: halfChunk] = phaseShifts
-# phases[0, halfChunk + 1: halfChunk + 1 + stringlen] = -phaseShifts[::-1]
+for i in range(len(phaseShifts)):
+    x = np.linspace(0, FREQ_0 * 2 * np.pi * (NPERSEG * .5 / sampling_rate), int(NPERSEG * .5))
+    x = np.sin(x + phaseShifts[i])
+    # print(x)
+    signal[int((i + .25) * NPERSEG) : int((i + .75) * NPERSEG)] += x * AMPLITUDE
 
-print(phases)
+# test = signal.tolist()
+# for i in range(100):
+#     print(test[i])
 
-# chunks = (magnitudes * np.exp(1j * phases))
-# chunks = np.fft.ifft(chunks).real
-# signal[0] = chunks.ravel().astype(np.int16) 
-# wavfile.write("modified_" + AUDIO_FILE_NAME, sampling_rate, signal.T)
+audiofile.write("modified_" + AUDIO_FILE_NAME.replace("mp3", "wav"), signal, sampling_rate)
