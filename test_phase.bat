83b5ae7795fcdc632160efc8f50d038ba638bd93
Simon Shkolnik <simonjs830@gmail.com>
2024-06-10 03:47:54 -0400

    update bat file

--- test_phase.bat
+++ test_phase.bat
@@ -2,11 +2,11 @@ set STRING=sample test text 123456
 
 echo %STRING% > _temp.txt
 
-python phaseEncode.py sample.wav _temp.txt
+python phaseEncode.py ImperialMarch60.wav _temp.txt
 
 echo Expecting "%STRING%"
 
-python phaseDecode.py modified_sample.wav
+python phaseDecode.py modified_ImperialMarch60.wav
 
 del _temp.txt
 //del modified_ImperialMarch60.wav
